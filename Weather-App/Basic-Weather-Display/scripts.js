b7f6d43be8a8fb250b1d35fb66eb0ce23e88c172
ChrisBerry1234 <christopherjberry846@gmail.com>
2025-10-20 14:03:16 -0400

    Rendering Text due to select option-JavaScript

--- Weather-App/Basic-Weather-Display/scripts.js
+++ Weather-App/Basic-Weather-Display/scripts.js
@@ -7,13 +7,33 @@ let content = document.getElementById("information-weather-display")
 function displayMonth(){
     const choice = monthInput.value;
     
-    if(choice === 'December'){
-        content.textContent = "Hey";
-    }
-    else{
-        console.log("Waiting");
+   switch (choice) {
+    case "January":
+    case "Feburary":
+    case "December":
+        content.textContent = " Cold and snowy — winter chill fills the air.";
+        break;
+
+    case "March":
+    case "April":
+    case "May":
+        content.textContent = "Mild and breezy — spring blossoms and fresh air.";
+        break;
+
+    case "June":
+    case "July":
+    case "August":
+        content.textContent = "Hot and sunny — summer days full of warmth";
+        break;
+
+    case "September":
+    case "October":
+    case "November":
+        content.textContent = "Cool and crisp 🍂 — autumn leaves and cozy evenings.";
+        break;
+
+    default:
         content.textContent = '';
-    }
-   
+   }
 }
 
